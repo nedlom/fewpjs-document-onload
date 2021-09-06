ac86323b8fe39651cf4470e6d0cfd2a043346928
Nick <nelom3@gmail.com>
2021-09-06 13:27:18 -0700

    commit tree 587598a8e75ab493831beb6ea5ab6d7d5ae6a6fb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  let paragraph = document.getElementById("text")
+  paragraph.innerHTML = "This is really cool!"
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
